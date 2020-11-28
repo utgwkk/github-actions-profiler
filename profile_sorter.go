2ee2d178eebe51116da7bdd06a43b6ce649c831f
utgwkk <utagawakiki@gmail.com>
2020-11-28 18:43:48 +0900

    sort_by

--- profile_sorter.go
+++ profile_sorter.go
@@ -1,6 +1,18 @@
 package ghaprofiler
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+var availableSortFields = []string{
+	"number",
+	"min",
+	"max",
+	"mean",
+	"median",
+}
 
 type taskStepProfileSorter struct {
 	taskStepProfiles []*TaskStepProfile
@@ -27,4 +39,53 @@ func (ts *taskStepProfileSorter) Swap(i, j int) {
 
 func (ts *taskStepProfileSorter) Less(i, j int) bool {
 	return ts.by(ts.taskStepProfiles[i], ts.taskStepProfiles[j])
+}
+
+func SortProfileBy(profile TaskStepProfileResult, fieldName string) error {
+	var by taskStepProfileSortBy
+
+	switch fieldName {
+	case "number":
+		by = func(t1, t2 *TaskStepProfile) bool {
+			return t1.Number < t2.Number
+		}
+		break
+	case "min":
+		by = func(t1, t2 *TaskStepProfile) bool {
+			return t1.Min < t2.Min
+		}
+		break
+	case "max":
+		by = func(t1, t2 *TaskStepProfile) bool {
+			return t1.Max < t2.Max
+		}
+		break
+	case "mean":
+		by = func(t1, t2 *TaskStepProfile) bool {
+			return t1.Mean < t2.Mean
+		}
+		break
+	case "median":
+		by = func(t1, t2 *TaskStepProfile) bool {
+			return t1.Median < t2.Median
+		}
+		break
+	default:
+		return fmt.Errorf("Invalid field: %s", fieldName)
+	}
+	taskStepProfileSortBy(by).Sort(profile)
+	return nil
+}
+
+func AvailableSortFieldsForCLI() string {
+	return strings.Join(availableSortFields, ", ")
+}
+
+func IsValidSortFieldName(fieldName string) bool {
+	for _, availableName := range availableSortFields {
+		if fieldName == availableName {
+			return true
+		}
+	}
+	return false
 }

ba9cb0141c87f58976927f9796a874c4e7f0a2f3
utgwkk <utagawakiki@gmail.com>
2020-11-28 21:22:40 +0900

    fix test

--- config_test.go
+++ config_test.go
@@ -2,9 +2,13 @@ package ghaprofiler
 
 import "testing"
 
-func Test_DefaultProfileConfigPassesValidation(t *testing.T) {
-	config := DefaultProfileConfig()
-	err := config.Validate()
+func Test_Validate(t *testing.T) {
+	config, err := LoadConfigFromTOML("fixtures/valid-config.toml")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = config.Validate()
 	if err != nil {
 		t.Fatalf("Validation failed for ghaprofiler.DefaultProfileConfig(): %v", err)
 	}

b0e039c52258cb559b4b7aba5e14700a4dab8856
utgwkk <utagawakiki@gmail.com>
2020-11-29 23:54:23 +0900

    filter for table/markdown

--- formatter.go
+++ formatter.go
@@ -99,7 +99,17 @@ func WriteJSON(w io.Writer, profileResult ProfileInput) (err error) {
 	return
 }
 
-func WriteTable(w io.Writer, profileResult ProfileInput, markdown bool) error {
+func showHeader(showIf filterFieldFunc) []string {
+	var header []string
+	for _, field := range fields {
+		if showIf(field) {
+			header = append(header, field)
+		}
+	}
+	return header
+}
+
+func WriteTable(w io.Writer, profileResult ProfileInput, markdown bool, showIf filterFieldFunc) error {
 	for _, p := range profileResult {
 		table := tablewriter.NewWriter(w)
 		table.SetAutoFormatHeaders(false)
@@ -108,20 +118,40 @@ func WriteTable(w io.Writer, profileResult ProfileInput, markdown bool) error {
 			table.SetCenterSeparator("|")
 			table.SetAutoWrapText(false)
 		}
-		table.SetHeader([]string{"Number", "Min", "Median", "Mean", "P50", "P90", "P95", "P99", "Max", "Name"})
+		table.SetHeader(showHeader(showIf))
 		for _, p := range p.Profile {
-			table.Append([]string{
-				strconv.FormatInt(p.Number, 10),
-				strconv.FormatFloat(p.Min, 'f', 6, 64),
-				strconv.FormatFloat(p.Median, 'f', 6, 64),
-				strconv.FormatFloat(p.Mean, 'f', 6, 64),
-				strconv.FormatFloat(p.Percentile50, 'f', 6, 64),
-				strconv.FormatFloat(p.Percentile90, 'f', 6, 64),
-				strconv.FormatFloat(p.Percentile95, 'f', 6, 64),
-				strconv.FormatFloat(p.Percentile99, 'f', 6, 64),
-				strconv.FormatFloat(p.Max, 'f', 6, 64),
-				p.Name,
-			})
+			var data []string
+			if showIf("number") {
+				data = append(data, strconv.FormatInt(p.Number, 10))
+			}
+			if showIf("min") {
+				data = append(data, strconv.FormatFloat(p.Min, 'f', 6, 64))
+			}
+			if showIf("median") {
+				data = append(data, strconv.FormatFloat(p.Median, 'f', 6, 64))
+			}
+			if showIf("mean") {
+				data = append(data, strconv.FormatFloat(p.Mean, 'f', 6, 64))
+			}
+			if showIf("percentile50") {
+				data = append(data, strconv.FormatFloat(p.Percentile50, 'f', 6, 64))
+			}
+			if showIf("percentile90") {
+				data = append(data, strconv.FormatFloat(p.Percentile90, 'f', 6, 64))
+			}
+			if showIf("percentile95") {
+				data = append(data, strconv.FormatFloat(p.Percentile95, 'f', 6, 64))
+			}
+			if showIf("percentile99") {
+				data = append(data, strconv.FormatFloat(p.Percentile99, 'f', 6, 64))
+			}
+			if showIf("max") {
+				data = append(data, strconv.FormatFloat(p.Max, 'f', 6, 64))
+			}
+			if showIf("name") {
+				data = append(data, p.Name)
+			}
+			table.Append(data)
 		}
 		if markdown {
 			fmt.Fprintf(w, "# Job: %s\n", p.Name)
@@ -153,10 +183,10 @@ func WriteWithFormat(w io.Writer, profileResult ProfileInput, format string) err
 		WriteJSON(w, profileResult)
 		break
 	case formatNameTable:
-		WriteTable(w, profileResult, false)
+		WriteTable(w, profileResult, false, func(_ string) bool { return true })
 		break
 	case formatNameMarkdown:
-		WriteTable(w, profileResult, true)
+		WriteTable(w, profileResult, true, func(_ string) bool { return true })
 		break
 	case formatNameTSV:
 		WriteTSV(w, profileResult)

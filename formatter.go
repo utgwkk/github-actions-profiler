3a51dad1fd1cc2a216afd50018cbfd9c10414ea1
utgwkk <utagawakiki@gmail.com>
2020-11-29 21:11:23 +0900

    better table representation

--- formatter.go
+++ formatter.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 const (
@@ -49,11 +52,25 @@ func WriteJSON(w io.Writer, profileResult ProfileInput) (err error) {
 
 func WriteTable(w io.Writer, profileResult ProfileInput) error {
 	for _, p := range profileResult {
-		fmt.Fprintf(w, "Job: %s\n", p.Name)
-		fmt.Fprintln(w, "Number\tMin\tMedian\tMean\tP50\tP90\tP95\tP99\tMax\tName")
+		table := tablewriter.NewWriter(w)
+		table.SetAutoFormatHeaders(false)
+		table.SetHeader([]string{"Number", "Min", "Median", "Mean", "P50", "P90", "P95", "P99", "Max", "Name"})
 		for _, p := range p.Profile {
-			fmt.Fprintf(w, "%d\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%s\n", p.Number, p.Min, p.Median, p.Mean, p.Percentile50, p.Percentile90, p.Percentile95, p.Percentile99, p.Max, p.Name)
+			table.Append([]string{
+				strconv.FormatInt(p.Number, 10),
+				strconv.FormatFloat(p.Min, 'f', 6, 64),
+				strconv.FormatFloat(p.Median, 'f', 6, 64),
+				strconv.FormatFloat(p.Mean, 'f', 6, 64),
+				strconv.FormatFloat(p.Percentile50, 'f', 6, 64),
+				strconv.FormatFloat(p.Percentile90, 'f', 6, 64),
+				strconv.FormatFloat(p.Percentile95, 'f', 6, 64),
+				strconv.FormatFloat(p.Percentile99, 'f', 6, 64),
+				strconv.FormatFloat(p.Max, 'f', 6, 64),
+				p.Name,
+			})
 		}
+		fmt.Fprintf(w, "Job: %s\n", p.Name)
+		table.Render()
 		fmt.Fprintln(w)
 	}
 	return nil
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/montanaflynn/stats v0.6.3
+	github.com/olekukonko/tablewriter v0.0.4
 	github.com/pelletier/go-toml v1.8.1
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pkg/errors v0.9.1

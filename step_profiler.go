ca162c408750beafa24a9e73fec4076273eada13
utgwkk <utagawakiki@gmail.com>
2020-12-02 22:10:41 +0900

    change format of JSON

--- step_profiler.go
+++ step_profiler.go
@@ -1,22 +1,46 @@
 package ghaprofiler
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/google/go-github/v32/github"
 	"github.com/montanaflynn/stats"
 	"github.com/pkg/errors"
 )
 
 type TaskStepProfile struct {
-	Name         string  `json:"name"`
-	Number       int64   `json:"number"`
-	Min          float64 `json:"min"`
-	Max          float64 `json:"max"`
-	Median       float64 `json:"median"`
-	Mean         float64 `json:"mean"`
-	Percentile50 float64 `json:"50percentile"`
-	Percentile90 float64 `json:"90percentile"`
-	Percentile95 float64 `json:"95percentile"`
-	Percentile99 float64 `json:"99percentile"`
+	Name        string                    `json:"name"`
+	Number      int64                     `json:"number"`
+	Min         float64                   `json:"min"`
+	Max         float64                   `json:"max"`
+	Median      float64                   `json:"median"`
+	Mean        float64                   `json:"mean"`
+	Percentiles map[int64]*percentileData `json:"percentiles"`
+}
+
+var percentiles = []int64{
+	50,
+	90,
+	95,
+	99,
+}
+
+type percentileData struct {
+	Percentile int64
+	Value      float64
+}
+
+func (pd *percentileData) MarshalJSON() ([]byte, error) {
+	return json.Marshal(pd.Value)
+}
+
+func (pd percentileData) Label() string {
+	return fmt.Sprintf("P%d", pd.Percentile)
+}
+
+func (pd percentileData) SortKey() string {
+	return fmt.Sprintf("p%d", pd.Percentile)
 }
 
 type TaskStepProfileResult = []*TaskStepProfile
@@ -55,34 +79,23 @@ func ProfileTaskStep(steps []*github.TaskStep) (profileResult TaskStepProfileRes
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to calculate mean")
 		}
-		percentile99, err := stats.Percentile(stepElapsed, 99)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to calculate 99%ile")
-		}
-		percentile95, err := stats.Percentile(stepElapsed, 95)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to calculate 95%ile")
-		}
-		percentile90, err := stats.Percentile(stepElapsed, 90)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to calculate 90%ile")
-		}
-		percentile50, err := stats.Percentile(stepElapsed, 50)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to calculate 50%ile")
+		percentileResult := map[int64]*percentileData{}
+		for _, percentile := range percentiles {
+			value, err := stats.Percentile(stepElapsed, float64(percentile))
+			if err != nil {
+				return nil, errors.Wrap(err, fmt.Sprintf("failed to calculate %d%%ile", percentile))
+			}
+			percentileResult[percentile] = &percentileData{Percentile: percentile, Value: value}
 		}
 
 		profileResult = append(profileResult, &TaskStepProfile{
-			Name:         *stepName,
-			Number:       stepNumber,
-			Min:          min,
-			Max:          max,
-			Median:       median,
-			Mean:         mean,
-			Percentile50: percentile50,
-			Percentile90: percentile90,
-			Percentile95: percentile95,
-			Percentile99: percentile99,
+			Name:        *stepName,
+			Number:      stepNumber,
+			Min:         min,
+			Max:         max,
+			Median:      median,
+			Mean:        mean,
+			Percentiles: percentileResult,
 		})
 	}
 
